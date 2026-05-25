f1340ec3ffb3863f9b7612f464fc007f7c3bd3a1
Zubair <zubairsanaullah1122@gmail.com>
2026-05-25 13:27:07 +0500

    Completed Home Doctor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar"
 import Banner from "../components/Banner"
 import Certifications from "../components/Certifications"
+import HomeDoctors from "../components/HomeDoctors"
 
 const Home = () => (
     <>
         <Navbar />
         <Banner />
         <Certifications />
+        <HomeDoctors />
     </>
 )
 
