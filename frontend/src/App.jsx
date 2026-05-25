2557516ab9abbb5b3739799cdd66fc80f32a0d5a
Zubair <zubairsanaullah1122@gmail.com>
2026-05-25 17:28:48 +0500

    Pending

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { Doctor } from './pages/Doctor'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctor />} />
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/Doctor.jsx
@@ -0,0 +1,13 @@
+import DoctorPage from "../components/DoctorPage"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+export const Doctor = () => {
+    return (
+        <div>
+            <Navbar />
+            <DoctorPage />
+            <Footer />
+        </div>
+    )
+}
