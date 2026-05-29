fa606397f27ba50d75d17a2bb0a1daf792597560
Zubair Sanaullah <zubairsanaullah1122@gmail.com>
2026-05-30 02:03:01 +0500

    Completed Appointment Page

---
+++ frontend/src/pages/Appointments.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import AppointmentPage from '../components/AppointmentPage'
+
+const Appointments = () => {
+    return (
+        <>
+            <Navbar />
+            <AppointmentPage />
+            <Footer />
+        </>
+    )
+}
+
+export default Appointments
